9d487477204a0f02b3b1f8beff182fc3758537f2
araysac <isaacarayaq@gmail.com>
2026-09-11 16:16:17 -0600

    subiendo funcion gramatica para as set

--- Proyecto1_RutasEnInternet/Avance3/parser.py
+++ Proyecto1_RutasEnInternet/Avance3/parser.py
@@ -179,10 +179,13 @@ def p_as_path_single(p):
     'as_path : as_element'
     p[0] = [p[1]]
 
+def p_as_set(p):
+    'as_set : LBRACE as_set_list RBRACE'
+    p[0] = set(p[2])
+
 def p_as_set_list_multiple(p):
     'as_set_list : NUMBER COMMA as_set_list'
     p[0] = [p[1]] + p[3]
-
 
 def p_as_set_list_single(p):
     'as_set_list : NUMBER'
